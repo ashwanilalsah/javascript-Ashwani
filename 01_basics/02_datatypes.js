4fd583a62205dbcd71b94f9796e574457bdc1cf5
Ashwani Lal Sah <159474446+ashwanilalsah@users.noreply.github.com>
2025-03-15 10:28:49 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,38 @@
+"use srtict" ; // Treat all js code as newer vesrion
+
+//alert(3+3) we are using node js not browser(open in documents)
+
+console.log(3
+    
+    +3) // code readability should be high 
+
+console.log("Ashwani")
+
+let name = "Ashwani"
+let age = 22
+
+let isloggedIn = false
+
+//number-> 2 to power 53
+
+//biginit ->
+
+//string = " "
+
+//boolean -> true/false
+
+//null -> standalone value
+
+//undefined -> 
+
+//symbol -> unique
+
+//object
+
+console.log(typeof "Ashwani")
+
+console.log(typeof age)
+
+console.log(typeof null)       //oblect
+
+console.log(typeof undefined)        //undefined
