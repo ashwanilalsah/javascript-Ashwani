15d36d9496cf41a85a0d8392cfa594b8e648d9ce
Ashwani Lal Sah <159474446+ashwanilalsah@users.noreply.github.com>
2025-03-15 10:28:04 +0000

    conversion

---
+++ 01_basics/03_conversionoperations.js
@@ -0,0 +1,33 @@
+let score = "Ashwani"
+
+console.log(typeof score)    //write in both way like we write down
+
+console.log(typeof (score))    // write in this way also
+
+let valueinnumber = Number(score)
+
+console.log(typeof valueinnumber)
+console.log(valueinnumber)
+ 
+//"11" -> 11
+
+//"Ashwani" -> Nan(not a number)
+
+//true -> 1 , false -> 0
+
+let isloggedIn = "hitesh"
+
+let booleanisloggedIn = Boolean(isloggedIn)
+
+console.log(typeof booleanisloggedIn)
+console.log(booleanisloggedIn)
+
+// 1-> true , 0-> false
+// " " -> false
+//" Ashwani" -> True
+
+let somenumber =33 
+let stringnumber = String(somenumber)
+
+console.log(stringnumber)
+console.log(typeof stringnumber)
