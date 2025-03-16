374aa2a7c1f0429e9c13da46bab9de22c550e42a
Ashwani Lal Sah <159474446+ashwanilalsah@users.noreply.github.com>
2025-03-16 10:30:19 +0000

    operationsm

--- 01_basics/03_conversionoperations.js
+++ 01_basics/03_conversionoperations.js
@@ -1,33 +1,93 @@
-let score = "Ashwani"
+// // let score = "Ashwani"
 
-console.log(typeof score)    //write in both way like we write down
+// // console.log(typeof score)    //write in both way like we write down
 
-console.log(typeof (score))    // write in this way also
+// // console.log(typeof (score))    // write in this way also
 
-let valueinnumber = Number(score)
+// // let valueinnumber = Number(score)
 
-console.log(typeof valueinnumber)
-console.log(valueinnumber)
+// // console.log(typeof valueinnumber)
+// // console.log(valueinnumber)
  
-//"11" -> 11
+// // //"11" -> 11
 
-//"Ashwani" -> Nan(not a number)
+// // //"Ashwani" -> Nan(not a number)
 
-//true -> 1 , false -> 0
+// // //true -> 1 , false -> 0
 
-let isloggedIn = "hitesh"
+// // let isloggedIn = "hitesh"
 
-let booleanisloggedIn = Boolean(isloggedIn)
+// // let booleanisloggedIn = Boolean(isloggedIn)
 
-console.log(typeof booleanisloggedIn)
-console.log(booleanisloggedIn)
+// // console.log(typeof booleanisloggedIn)
+// // console.log(booleanisloggedIn)
 
-// 1-> true , 0-> false
-// " " -> false
-//" Ashwani" -> True
+// // // 1-> true , 0-> false
+// // // " " -> false
+// // //" Ashwani" -> True
 
-let somenumber =33 
-let stringnumber = String(somenumber)
+// // let somenumber =33 
+// // let stringnumber = String(somenumber)
 
-console.log(stringnumber)
-console.log(typeof stringnumber)
+// // console.log(stringnumber)
+// // console.log(typeof stringnumber)
+
+// ------------------------------------------------------------------------------------------------
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue)
+
+// console.log(2*2);
+
+// console.log(2+2);
+
+// console.log(2-2);
+
+// console.log(2**3);  // 2 to the power 3
+
+// console.log(2/3);
+
+// console.log(2%3);
+
+// console.log((2+2) * 2 % 3); //code readability shoukd be high
+
+let str1 = "Hello"
+let str2 = " Ashwani"
+
+str3 = str1 + str2
+
+console.log(str3)
+
+console.log("2" + 3)
+
+console.log(2 + "3")
+
+console.log("2" + "3")
+
+console.log("2" + 2 + 3)
+
+console.log(2+ 2 + "3")    // NOTE
+
+console.log(true)
+
+console.log(+true)          //    +  is used for conversion
+
+//console.log(true+)
+
+console.log (false)
+
+console.log(+false)
+
+
+console.log(+ " ")
+
+let gamecounter = 100
+
+gamecounter++                        //post-increment
+
+console.log(gamecounter)
+
+++gamecounter                         //pre-increment
+
+console.log(gamecounter)
