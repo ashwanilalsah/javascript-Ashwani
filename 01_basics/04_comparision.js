8a1709c0edf6378027f9e531dae6bca5edeff4cf
Ashwani Lal Sah <159474446+ashwanilalsah@users.noreply.github.com>
2025-03-16 10:56:52 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,27 @@
+// // console.log( 2 >= 3)
+// // console.log(2==3)
+// // console.log(3>4)
+// // console.log(3<4)
+// // // console.log(3!=4)
+
+// console.log("2" < 3)
+
+// console.log("02" < 3)
+
+// console.log("2" > 3)
+
+// console.log(null == 0)
+// console.log(null > 0)
+// console.log(null >= 0) //show true becouse ==, >, < and >= works differently as >= take null value a 0
+
+// console.log(undefined == 0)
+// console.log(undefined > 0)
+// console.log(undefined < 0)
+// console.log(undefined >= 0)        //all value show false as output
+
+
+// // ===  -> use to convert in datatypes
+
+console.log("2" === 2)       // it shows false becouse "_" is not a integer, === change it in datatypes
+
+console.log("2" == 2)       // it shows true value as "==" didnot convert number inti datatypes
