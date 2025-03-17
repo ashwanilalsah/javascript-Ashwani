01fad6c6998e881680ca878b76f4c461b854d92e
Ashwani Lal Sah <159474446+ashwanilalsah@users.noreply.github.com>
2025-03-17 11:42:45 +0000

    stack and heap

---
+++ 01_basics/stackheaps.js
@@ -0,0 +1,23 @@
+//     Heap memoy(premitive deatypes) ,     Stack memory(non-premitive)
+
+
+let myname = "Ashwani"
+let myanotherbname = myname
+
+myanothername = "Badshah"
+
+console.log(myname)
+
+console.log(myanothername)
+
+let user = {
+    email : "ashwani@google.com",
+    upi : "ashwani@ybl"
+}
+
+let usertwo = user
+usertwo.email = "abc@google.com"  // reference of stack memory is heap as user data is store in heap and usertwo take reference from user
+
+
+console.log(user.email)
+console.log(usertwo.email)
