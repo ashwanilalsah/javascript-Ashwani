022e69a7116a136726bf8b63e028bfc996c6b7fb
Ashwani Lal Sah <159474446+ashwanilalsah@users.noreply.github.com>
2025-04-08 08:58:20 +0000

    swtch

---
+++ 04_controlflow/switch.js
@@ -0,0 +1,48 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+// const month = 3
+// switch (month) {
+//     case 1:
+//         console.log("January")
+//         break;
+//     case 2:
+//         console.log("fabruary")
+//         break;
+//     case 3:
+//         console.log("march")
+//         break;       // if we comment break then this case execut all code except default
+//     case 4:
+//         console.log("april")
+//         break;
+
+//     default:
+//         console.log("Default case matched")
+//         break;
+// }
+const month = "march"
+switch (month) {
+    case "january":
+        console.log("January")
+        break;
+    case "february":
+        console.log("fabruary")
+        break;
+    case "march":
+        console.log("march")
+        break;       // if we comment break then this case execut all code except default
+    case "april":
+        console.log("april")
+        break;
+
+    default:
+        console.log("Default case matched")
+        break;
+}
