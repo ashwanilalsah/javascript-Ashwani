bb67d079dfa6a261af4a813a5fc90ba7dee6cb69
Ashwani Lal Sah <159474446+ashwanilalsah@users.noreply.github.com>
2025-04-08 08:26:04 +0000

    if else

---
+++ 04_controlflow/one.js
@@ -0,0 +1,35 @@
+                            //IF
+const isuserloggedin = true
+
+if(2 == "2")
+    {
+        console.log("executed")
+    }
+const  temperature = 51
+if(temperature === 51){
+    console.log("temperature found")
+}
+else{
+    console.log("not found")
+}
+console.log("Got the temperature")
+
+const power = 200
+if(power > 100){
+    const score = "fly"
+    console.log(`user score: ${score}`)
+}
+
+const balance = 2000
+if(balance>100) console.log("test1"), console.log("test2")
+
+const userloggedin = true
+const debitcard = true
+
+if(userloggedin && debitcard)
+// if(userloggedin || debitcard)
+{
+    console.log("allow to buy")
+}
+
+//use of if else elseif is same as coding language
