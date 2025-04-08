9d0524ac83356f47c7b9c9794a92a60e244ce6ca
Ashwani Lal Sah <159474446+ashwanilalsah@users.noreply.github.com>
2025-04-08 21:58:05 +0000

    while

---
+++ 05_iteration/two.js
@@ -0,0 +1,18 @@
+let index = 0
+while (index <= 20) {
+    // console.log(`value of index is ${index}`)
+    index = index + 2
+}
+
+let myArray = ['Ashwani', 'Badshah', 'sabkepappaji']
+let arr =0
+while (arr<myArray.length) {
+    console.log(`value is ${myArray[arr]}`)
+    arr = arr + 1
+}
+
+let score = 21
+do {
+    console.log(`value is ${score}`)
+    
+} while (score < 20);
